275c93bdc3626c751aa50814bf8ae540aff14ce4
Ed Slavich <eslavich@stsci.edu>
2019-08-26 14:44:58 -0400

    With a volume

--- aws_spawner/spawner.py
+++ aws_spawner/spawner.py
@@ -3,6 +3,7 @@ import logging
 import json
 
 from jupyterhub.spawner import Spawner
+from jupyterhub.traitlets import ByteSpecification
 from traitlets import (
     Bool,
     Dict,
@@ -16,7 +17,6 @@ from traitlets import (
 )
 import boto3
 
-LOGGER = logging.getLogger(__name__)
 
 class AwsSpawner(Spawner):
     def __init__(self, *args, **kwargs):
@@ -29,83 +29,121 @@ class AwsSpawner(Spawner):
     launch_template_id = Unicode(
         # TODO: Make the help more helpful
         help="""
-        AWS LaunchTemplateId
+        AWS LaunchTemplateId that defines the instance created.
+        """
+    ).tag(config=True)
+
+    instance_type = Unicode(
+        help="""
+        EC2 instance type that will be be created.  If absent, the launch template's default
+        will be used.
+        """
+    ).tag(config=True)
+
+    home_volume_size = ByteSpecification(
+        # TODO: None, or no argument here?
+        None,
+        help="""
+        Size of the EBS volume that will be created for the user's home directory.
+        """,
+    ).tag(config=True)
+
+    availability_zone = Unicode(
+        help=-"""
+        Availability zone of spawned instances and volumes
         """
     ).tag(config=True)
 
     async def start(self):
-        LOGGER.debug("Entered start")
+        self.log.debug("Entered start")
         # TODO: Devise tag scheme with username
-
-        LOGGER.debug("Looking for volume")
 
         user_data = self.get_user_data()
 
-        LOGGER.debug("User data: %s", user_data)
-        LOGGER.debug("User: %s", self.user)
+        self.log.debug("User data: %s", user_data)
+        self.log.debug("User: %s", self.user)
 
-        instance = self.ec2.create_instances(
-            MinCount=1,
-            MaxCount=1,
-            LaunchTemplate={"LaunchTemplateId": self.launch_template_id},
-            UserData=user_data
-        )[0]
+        create_instances_kwargs = {
+            "MinCount": 1,
+            "MaxCount": 1,
+            "LaunchTemplate": {"LaunchTemplateId": self.launch_template_id},
+            "UserData": user_data
+        }
 
+        if self.instance_type:
+            create_instances_kwargs["InstanceType"] = self.instance_type
+
+        if self.availability_zone:
+            create_instances_kwargs["Placement"] = {"AvailabilityZone": self.availability_zone}
+
+        self.log.debug("Creating instance with %s", create_instance_kwargs)
+        instance = self.ec2.create_instances(**create_instances_kwargs)
         self.instance_id = instance.id
-
-        LOGGER.debug("Created instance_id %s", self.instance_id)
+        self.log.debug("Created instance_id %s", self.instance_id)
 
         # TODO: This needs to be smarter, check for unexpected codes, etc
         # https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_InstanceState.html
         while instance.state["Code"] != 16:
-            LOGGER.debug("Code is %s", instance.state["Code"])
+            self.log.debug("Code is %s", instance.state["Code"])
             await asyncio.sleep(1)
             instance.reload()
+
+        # TODO: SnapshotId would go here
+        create_volume_kwargs = {
+            "AvailabilityZone": instance.placement["AvailabilityZone"],
+        }
+        self.log.debug("Creating volume with %s", create_volume_kwargs)
+        volume = self.ec2.create_volume(**create_volume_kwargs)
+        self.log.debug("Created volume id %s", volume.id)
+        self.volume_id = volume.id
+        self.log.debug("Attaching volume")
+        attach_response = instance.attach_volume(VolumeId=volume.id)
+        self.log.debug("Volume attached with device %s", attach_response["Device"])
 
         # TODO: Check "Status Checks" instead of just Instance State
 
         self.ip_address = instance.network_interfaces[0].private_ip_addresses[0]["PrivateIpAddress"]
 
-        LOGGER.debug("Returning IP address %s, port %s", self.ip_address, self.port)
+        self.log.debug("Returning IP address %s, port %s", self.ip_address, self.port)
 
         return self.ip_address, self.port
 
     async def poll(self):
-        LOGGER.debug("Entered poll")
+        self.log.debug("Entered poll")
         # TODO: Return status 0 if not started yet
 
         if self.instance_id is not None:
-            LOGGER.debug("Returning None")
+            self.log.debug("Returning None")
             return None
         else:
-            LOGGER.debug("Returning 0")
+            self.log.debug("Returning 0")
             return 0
 
     async def stop(self, now=False):
-        LOGGER.debug("Entered stop")
+        self.log.debug("Entered stop")
 
         if self.instance_id is None:
-            LOGGER.debug("No instance_id, returning from stop")
+            self.log.debug("No instance_id, returning from stop")
             return
 
-        LOGGER.debug("Terminating instance_id %s", self.instance_id)
+        self.log.debug("Terminating instance_id %s", self.instance_id)
         instance = self._get_instance(self.instance_id)
         if not instance:
-            LOGGER.warning("Missing instance %s", self.instance_id)
+            self.log.warning("Missing instance %s", self.instance_id)
         instance.terminate()
 
         while instance.state["Code"] != 48:
-            LOGGER.debug("Code is %s", instance.state["Code"])
+            self.log.debug("Code is %s", instance.state["Code"])
             await asyncio.sleep(1)
             instance.reload()
 
         self.instance_id = None
         self.ip_address = None
 
-        LOGGER.debug("Returning from stop")
+        self.log.debug("Returning from stop")
 
     def get_state(self):
-        LOGGER.debug("Getting state")
+        self.log.debug("Getting state")
 
         state = super().get_state()
 
@@ -114,12 +152,12 @@ class AwsSpawner(Spawner):
             "ip_address": self.ip_address
         })
 
-        LOGGER.debug("Returning state: %s", state)
+        self.log.debug("Returning state: %s", state)
 
         return state
 
     def load_state(self, state):
-        LOGGER.debug("Loading state: %s", state)
+        self.log.debug("Loading state: %s", state)
 
         super().load_state(state)
 
@@ -127,7 +165,7 @@ class AwsSpawner(Spawner):
         self.ip_address = state.get("ip_address")
 
     def clear_state(self):
-        LOGGER.debug("Clearing state")
+        self.log.debug("Clearing state")
 
         super().clear_state()
 
